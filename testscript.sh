7e9a8c9aaeb85b7469dbbaa00525866fa6392448
Cristiano Silva <cristiano.c.silva@accenture.com>
2022-10-10 16:23:20 -0300

    updating to hook automatic scm pool from jenkins - 2

--- testscript.sh
+++ testscript.sh
@@ -3,5 +3,5 @@
 echo "this is script from github"
 NUMBER=$(($NUMBERONE +$NUMBERTWO))
 echo "$NUMBER"
-echo "this is a first script - change for hook trigger - trying again"
+echo "this is a first script - change for hook trigger - trying again - again"
 
